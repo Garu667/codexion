1d9a59ba591a903f7f67a014b91020f699a6a0e0
Garu667 <lolpmmo@gmail.com>
2026-09-05 11:08:52 +0200

    simplify the heap.c file because there can be only 2 coders for a dongle

--- src/heap.c
+++ src/heap.c
@@ -21,47 +21,11 @@ static int	has_priority(t_waiter *a, t_waiter *b, int scheduler)
 	return (a->arrived_at < b->arrived_at);
 }
 
-static void	heap_bubble_up(t_dongle *dongle, int idx, int scheduler)
+t_waiter	*heap_peek(t_dongle *dongle)
 {
-	int	parent;
-
-	while (idx > 0)
-	{
-		parent = (idx - 1) / 2;
-		if (has_priority(dongle->queue[idx], dongle->queue[parent], scheduler))
-		{
-			swap(&dongle->queue[idx], &dongle->queue[parent]);
-			idx = parent;
-		}
-		else
-			return ;
-	}
-}
-
-static void	heap_bubble_down(t_dongle *dongle, int idx, int scheduler)
-{
-	int	left;
-	int	right;
-	int	prio;
-
-	while (1)
-	{
-		left = 2 * idx + 1;
-		right = 2 * idx + 2;
-		prio = idx;
-		if (left < dongle->queue_size
-			&& has_priority(dongle->queue[left],
-				dongle->queue[prio], scheduler))
-			prio = left;
-		if (right < dongle->queue_size
-			&& has_priority(dongle->queue[right],
-				dongle->queue[prio], scheduler))
-			prio = right;
-		if (prio == idx)
-			return ;
-		swap(&dongle->queue[idx], &dongle->queue[prio]);
-		idx = prio;
-	}
+	if (dongle->queue_size == 0)
+		return (NULL);
+	return (dongle->queue[0]);
 }
 
 void	heap_push(t_dongle *dongle, t_waiter *waiter, int scheduler)
@@ -70,7 +34,9 @@ void	heap_push(t_dongle *dongle, t_waiter *waiter, int scheduler)
 		return ;
 	dongle->queue[dongle->queue_size] = waiter;
 	dongle->queue_size++;
-	heap_bubble_up(dongle, dongle->queue_size - 1, scheduler);
+	if (dongle->queue_size == 2
+		&& has_priority(dongle->queue[1], dongle->queue[0], scheduler))
+		swap(&dongle->queue[0], &dongle->queue[1]);
 }
 
 t_waiter	*heap_pop(t_dongle *dongle, int scheduler)
@@ -82,7 +48,5 @@ t_waiter	*heap_pop(t_dongle *dongle, int scheduler)
 	top = dongle->queue[0];
 	dongle->queue_size--;
 	dongle->queue[0] = dongle->queue[dongle->queue_size];
-	if (dongle->queue_size > 0)
-		heap_bubble_down(dongle, 0, scheduler);
 	return (top);
 }
